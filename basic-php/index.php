ed66fbc34ebbe71330fc73aaa3fe94ad50070212
Julliard Macatuggal <86839197+Julliard-kun@users.noreply.github.com>
2025-09-05 21:17:27 +0800

    Initial commit for storing php projects
    Add folder for basic php folder then create index.php.

---
+++ basic-php/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "This is outside of the HTML tag.";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Basic PHP Page</title>
+    </head>
+    <body>
+        <h1><?php echo "Echo can be used inside the HTML tag"; ?></h1>
+
+
+    </body>
+
+</html>
