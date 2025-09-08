39f2b9666fb253b5a04741ee35654a1bdc89b785
Julliard Macatuggal <86839197+Julliard-kun@users.noreply.github.com>
2025-09-08 15:38:53 +0800

    Implement user registration functionality
    Added backend logic to register new users, including form validation and password hashing. Updated UI to display success and error messages, and fixed a typo in the registration prompt.

--- authentication-system/config.php
+++ authentication-system/config.php
@@ -9,12 +9,11 @@
 
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-    if ($conn == true) {
-        echo "Successfully connected to the database";
+    // if ($conn == true) {
+    //     echo "Successfully connected to the database";
 
-    } else {
-        echo "Failed to connect to the database";
-    }
- 
- 
+    // } else {
+    //     echo "Failed to connect to the database";
+    // }
+
  ?>
--- authentication-system/register.php
+++ authentication-system/register.php
@@ -1,8 +1,30 @@
 <?php require "includes/header.php"; ?>
+
+<?php require "config.php"; ?>
+
+<?php 
+    if(isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $register_query = "INSERT INTO users (email, username, password) VALUES (?, ?, ?)";
+        
+        if($email == "" || $username == "" || $password == "") {
+            $error = "All fields are required";
+
+        } else {
+            $stmt = $conn->prepare($register_query);
+            $stmt->execute([$email, $username, password_hash($password, PASSWORD_DEFAULT)]);
+            $success = "Account created successfully";
+        }
+    }
+?>
+
 <main class="form-signin w-50 m-auto">
     <form method="POST" action="register.php">
 
-        <h1 class="h3 mt-5 fw-normal text-center">Please Register</h1>
+        <h1 class="h3 mt-5 fw-normal text-center">Register an account?</h1>
 
         <div class="form-floating">
             <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -19,8 +41,13 @@
             <label for="floatingPassword">Password</label>
         </div>
 
+        <div class="form-floating">
+            <span class="text-danger"><?php if(isset($error)) echo $error; ?></span>
+            <span class="text-success"><?php if(isset($success)) echo $success; ?></span>
+        </div>
+
         <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">register</button>
-        <h6 class="mt-3">Aleardy have an account? <a href="login.php">Login</a></h6>
+        <h6 class="mt-3">Already have an account? <a href="login.php">Login</a></h6>
 
     </form>
 </main>
