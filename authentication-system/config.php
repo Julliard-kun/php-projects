9b289ef22beebecd32b897db8de397e27daed806
Julliard Macatuggal <86839197+Julliard-kun@users.noreply.github.com>
2025-09-08 11:48:18 +0800

    Add initial authentication system files
    Introduces the base structure for an authentication system, including configuration for database connection, header and footer includes, and basic index, login, and registration pages with Bootstrap styling.

---
+++ authentication-system/config.php
@@ -0,0 +1,20 @@
+<?php
+    $host = "localhost";
+
+    $dbname = "auth-sys";
+
+    $user = "root";
+
+    $pass = "";
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+    if ($conn == true) {
+        echo "Successfully connected to the database";
+
+    } else {
+        echo "Failed to connect to the database";
+    }
+ 
+ 
+ ?>
---
+++ authentication-system/index.php
@@ -0,0 +1,6 @@
+<?php require "includes/header.php"; ?>
+
+hello from index.php
+
+
+<?php require "includes/footer.php"; ?>
